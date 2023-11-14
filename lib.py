83e2298c00dfa8454a20c637f661a343d4f7fda1
Sander-317 <sander.van.meerveld@gmail.com>
2023-11-14 14:14:26 +0100

    added some xtra functions

--- lib.py
+++ lib.py
@@ -1,2 +1,11 @@
 def power(x, y):
     return x**y
+
+def division(x, y):
+    return x / y
+
+def create_full_name(first_name, last_name):
+    return f"{first_name} {last_name}"
+
+def multiply(x, y):
+    return x * y
--- test_lib.py
+++ test_lib.py
@@ -3,3 +3,15 @@ import lib
 def test_power():
     assert lib.power(2,2) == 4
     assert lib.power(2,3) == 8
+
+def test_division():
+    assert lib.division(10,2) == 5
+    assert lib.division(3,2) == 1.5
+
+def test_create_full_name():
+    assert lib.create_full_name("harry","pannekoek") == "harry pannekoek"
+    assert lib.create_full_name("bob","bouwer") == "bob bouwer"
+
+def test_multiply():
+    assert lib.multiply(10,2) == 20
+    assert lib.multiply(2,5) == 10
